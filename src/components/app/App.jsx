cf1844bed8180f317c6919d4292c2c8040dbd273
Magomed <mamergov01@bk.ru>
2022-01-12 18:11:19 +0300

    added header

---
+++ src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../'
+
+const App = () => {
+	return (
+		<div className="app">
+			<Header />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+const Header = () => {
+	return (
+		<Navbar bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="#home">Salads App</Navbar.Brand>
+				<Nav className="me-auto"></Nav>
+			</Container>
+		</Navbar>
+	)
+}
+
+export default Header
